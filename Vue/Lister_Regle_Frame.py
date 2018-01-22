b732a1f801b49bce770749cc0bf066125c7933c5
Wizkalista <kevinmaussion@gmail.com>
2018-01-22 22:10:31 +0100

    Avancée sur l'affichage des règles.

--- Vue/Lister_Regle_Frame.py
+++ Vue/Lister_Regle_Frame.py
@@ -11,15 +11,26 @@ class Lister_Regle_Frame(Frame):
         self.master.geometry('750x350')
 
         # Définition des frames
-        self.frame = Frame(fenetre)
-        self.frame.pack(side=TOP, fill=BOTH)
+        self.frame_top = Frame(fenetre, bg="blue")
+        self.frame_top.pack(side=TOP, fill=BOTH)
+
+        self.frame_bot = Frame(fenetre, bg="yellow")
+        self.frame_bot.pack(side=BOTTOM, fill=BOTH)
+
 
         liste_regle = ListeRegle()
         liste_regle.charger_liste_regle('NOMLOGICIEL.ini')
 
-        liste = Listbox(self.frame)
-        i = 0
-        for regle in liste_regle:
-            liste.insert(i, regle)
-            i += 1
-        liste.pack(fill=BOTH)
+        ###############Frame du haut###################################
+        # liste = Listbox(self.frame_top, selectmode=SINGLE)
+        # i = 0
+        # for regle in liste_regle:
+        #     liste.insert(i, regle)
+        #     i += 1
+        # liste.pack(fill=BOTH)
+        label = Label(self.frame_top, text="Règle choisie :")
+        label.pack(side=TOP)
+
+        ###############Frame du bas###################################
+        label = Label(self.frame_bot, text="Règle choisie :")
+        label.pack(side=TOP)
