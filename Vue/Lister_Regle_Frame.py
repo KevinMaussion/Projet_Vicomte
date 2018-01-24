5d38bc3f4926780412da29412aec06d593d06408
Wizkalista <kevinmaussion@gmail.com>
2018-01-25 00:31:38 +0100

    Affichage OK Events sur liste en cours

--- Vue/Lister_Regle_Frame.py
+++ Vue/Lister_Regle_Frame.py
@@ -12,25 +12,31 @@ class Lister_Regle_Frame(Frame):
 
         # Définition des frames
         self.frame_top = Frame(fenetre, bg="blue")
-        self.frame_top.pack(side=TOP, fill=BOTH)
+        self.frame_top.pack(side=TOP, fill=BOTH, expand=1)
 
         self.frame_bot = Frame(fenetre, bg="yellow")
-        self.frame_bot.pack(side=BOTTOM, fill=BOTH)
+        self.frame_bot.pack(side=BOTTOM, fill=BOTH, expand=1)
 
 
         liste_regle = ListeRegle()
         liste_regle.charger_liste_regle('NOMLOGICIEL.ini')
 
         ###############Frame du haut###################################
-        # liste = Listbox(self.frame_top, selectmode=SINGLE)
-        # i = 0
-        # for regle in liste_regle:
-        #     liste.insert(i, regle)
-        #     i += 1
-        # liste.pack(fill=BOTH)
-        label = Label(self.frame_top, text="Règle choisie :")
-        label.pack(side=TOP)
+        self.liste = Listbox(self.frame_top, selectmode=SINGLE)
+        i = 0
+        for regle in liste_regle:
+            self.liste.insert(i, regle)
+            i += 1
+        self.liste.pack(fill=BOTH)
+        self.liste.bind('<<ListboxSelect>>', self.callback)
+
+        # label = Label(self.frame_top, text="Règle choisie :")
+        # label.pack(side=TOP)
 
         ###############Frame du bas###################################
         label = Label(self.frame_bot, text="Règle choisie :")
         label.pack(side=TOP)
+
+    def callback(self, event):
+        label_regle = Label(self.frame_bot, text=self.liste.get(self.liste.curselection()))
+        label_regle.pack(side=BOTTOM)
