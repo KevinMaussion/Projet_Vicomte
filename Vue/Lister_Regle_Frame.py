57c8700c63348f1f7dfa5a6d8f21bc5b9bb10821
kmaussion <kevinmaussion@gmail.com>
2018-01-25 18:03:04 +0100

    Début travail transfert donnée entre frame.

--- Vue/Lister_Regle_Frame.py
+++ Vue/Lister_Regle_Frame.py
@@ -11,17 +11,16 @@ class Lister_Regle_Frame(Frame):
         self.master.geometry('750x350')
 
         # Définition des frames
-        self.frame_top = Frame(fenetre, bg="blue")
+        self.frame_top = Frame(fenetre)
         self.frame_top.pack(side=TOP, fill=BOTH, expand=1)
 
-        self.frame_bot = Frame(fenetre, bg="yellow")
+        self.frame_bot = Frame(fenetre)
         self.frame_bot.pack(side=BOTTOM, fill=BOTH, expand=1)
-
 
         liste_regle = ListeRegle()
         liste_regle.charger_liste_regle('NOMLOGICIEL.ini')
 
-        ###############Frame du haut###################################
+        # ##############Frame du haut###################################
         self.liste = Listbox(self.frame_top, selectmode=SINGLE)
         i = 0
         for regle in liste_regle:
@@ -30,13 +29,15 @@ class Lister_Regle_Frame(Frame):
         self.liste.pack(fill=BOTH)
         self.liste.bind('<<ListboxSelect>>', self.callback)
 
-        # label = Label(self.frame_top, text="Règle choisie :")
-        # label.pack(side=TOP)
-
-        ###############Frame du bas###################################
+        # ##############Frame du bas###################################
         label = Label(self.frame_bot, text="Règle choisie :")
         label.pack(side=TOP)
 
+        self.label_regle = Label(self.frame_bot, text="Aucune règle choisie.")
+        self.label_regle.pack(side=TOP)
+
+        self.button_validation = Button(self.frame_bot, text="Utiliser Règle")
+        self.button_validation.pack(side=BOTTOM)
+
     def callback(self, event):
-        label_regle = Label(self.frame_bot, text=self.liste.get(self.liste.curselection()))
-        label_regle.pack(side=BOTTOM)
+        self.label_regle.config(text=self.liste.get(self.liste.curselection()))
